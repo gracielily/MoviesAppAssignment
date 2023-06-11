df31b44eaaed7d0f7717de2c7e68ca458cfe8a56
gracelily <12722041@mail.wit.ie>
2023-06-11 12:45:40 +0100

    Refactor: Favourite movie list now stored in a context.

---
+++ src/contexts/moviesContext.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+export const MoviesContext = React.createContext(null);
+
+const MoviesContextProvider = (props) => {
+  const [favourites, setFavourites] = useState([]);
+
+  const addToFavourites = (movie) => {
+    let updatedFavourites = [...favourites];
+    if (!favourites.includes(movie.id)) {
+      updatedFavourites.push(movie.id);
+    }
+    setFavourites(updatedFavourites);
+  };
+
+  const removeFromFavourites = (movie) => {
+    setFavourites(favourites.filter((mId) => mId !== movie.id));
+  };
+
+  return (
+    <MoviesContext.Provider
+      value={{
+        favourites,
+        addToFavourites,
+        removeFromFavourites,
+      }}
+    >
+      {props.children}
+    </MoviesContext.Provider>
+  );
+};
+
+export default MoviesContextProvider;
--- src/pages/favouriteMoviesPage.jsx
+++ src/pages/favouriteMoviesPage.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import PageTemplate from "../components/templateMovieListPage";
+import { MoviesContext } from "../contexts/moviesContext";
+import { useQueries } from "react-query";
+import { getMovie } from "../api/tmdb-api";
+import Spinner from "../components/spinner";
 
 const FavouriteMoviesPage = (props) => {
+  const { favourites: movieIds } = useContext(MoviesContext);
+
+  const favouriteMovieQueries = useQueries(
+    movieIds.map((movieId) => {
+      return {
+        queryKey: ["movie", { id: movieId }],
+        queryFn: getMovie,
+      };
+    })
+  );
+  const isLoading = favouriteMovieQueries.find((m) => m.isLoading === true);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  const movies = favouriteMovieQueries.map((q) => q.data);
+
   const toDo = () => true;
-  // Get movies from local storage.
-  const movies = JSON.parse(localStorage.getItem("favourites")); 
 
   return (
     <PageTemplate
