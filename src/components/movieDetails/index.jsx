c8b585ddf788008c3abbc4eaac5b9607325480e6
gracelily <12722041@mail.wit.ie>
2023-06-11 12:13:31 +0100

    Refactor - useMovie custom hook.

--- src/components/movieDetails/index.jsx
+++ src/components/movieDetails/index.jsx
@@ -32,7 +32,7 @@ const styles = {
 };
 
 const MovieDetails = ( {movie}) => {
-  const [drawerOpen, setDrawerOpen] = useState(false); // New
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   return (
     <>
